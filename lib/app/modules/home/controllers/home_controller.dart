292544e4db30cde7378ad70550c8c8e64e4dc8b3
iheb meftah <ihebmeftah666@gmail.com>
2023-05-19 15:41:32 +0100

    remove actions

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  final GlobalKey<FormState> key = GlobalKey();
+  final TextEditingController formController = TextEditingController();
+  num? result;
+  void doOperation() {
+    if (formController.text.contains('+')) {
+      List op = formController.text.split("+");
+      result = num.parse(op.first) + num.parse(op.first);
+    }
+
+    update();
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,16 +9,47 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        body: Row(
+      children: [
+        const Spacer(),
+        Expanded(
+            flex: 8,
+            child: Form(
+              key: controller.key,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: controller.formController,
+                    decoration:
+                        const InputDecoration(border: OutlineInputBorder()),
+                    validator: (value) {
+                      if (value!.contains(RegExp(r'[+]')) == false ||
+                          value.isEmpty) {
+                        return "this not a math operation";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  GetBuilder<HomeController>(
+                    builder: (controller) =>
+                        Text("Result = ${controller.result ?? "no"}  "),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (controller.key.currentState!.validate()) {
+                          controller.doOperation();
+                        } else {}
+                      },
+                      child: const Text("Do operation"))
+                ],
+              ),
+            )),
+        const Spacer(),
+      ],
+    ));
   }
 }
